007a0a36e6a59fa6872d9828e41a6be42631b639
fathi-fayez <fathifayez155@gmail.com>
2024-12-21 14:43:45 +0200

    update responsive design for cart page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,6 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
-<<<<<<< HEAD
   readonly BASE_URL: string
-=======
-    readonly BASE_URL: string
->>>>>>> a46dd755974ed2e915c67b239efb4d0159632ab9
 }
 
 interface ImportMeta {
